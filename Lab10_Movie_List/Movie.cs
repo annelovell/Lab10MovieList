22dca57d5e8ad786955c1efa6486c5e6e1517e45
anne <amlovell2@outlook.com>
2021-05-05 18:31:19 -0400

    Lab 10 Movie list

---
+++ Lab10_Movie_List/Movie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10_Movie_List
+{
+    class Movie
+    {
+               //fields
+        private string category;
+        private string title;
+
+        //properties
+    public string Category { get; set; }
+       
+    public string Title { get; set; }
+        
+        //constructors
+        public Movie()
+        {
+
+        }
+
+    public Movie(string _category, string _title)
+        {
+            category = _category;
+            title = _title;
+                        
+        }
+    
+        public void PrintInfo()
+        {
+            Console.WriteLine("Category: " + category);
+           // Console.WriteLine("Title: " + title);
+        }
+    }
+}
---
+++ Lab10_Movie_List/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab10_Movie_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                // Category
+            Movie TheLionKing = new Movie("Animated", "The Lion King");
+            Movie ABugsLife = new Movie("Animated", "A Bugs Life");
+            Movie FindingNemo = new Movie("Animated", "Finding Nemo");
+            Movie ToyStory = new Movie("Animated", "Toy Story");
+            Movie Aladdin = new Movie("Animated", "Aladdin");
+            
+            Movie GoneWithTheWind = new Movie("Drama", "Gone With The Wind");
+            Movie SavingPrivateRyan = new Movie("Drama", "Saving Private Ryan");
+            Movie TheGreatGatsby = new Movie("Drama", "The Great Gatsby");
+            Movie ARiverRunsThroughIt = new Movie("Drama", "A River Runs Through It");
+            Movie TheColorPurple = new Movie("Drama", "The Color Purple");
+                        
+            Movie Halloween = new Movie("Horror", "Halloween");
+            Movie ANightmareOnElmStreet = new Movie("Horror", "A Nightmare On Elm Street");
+            Movie Fridaythe13th = new Movie("Horror", "Friday The 13th");
+            Movie Hellraiser = new Movie("Horror", "Hellraiser");
+            Movie IknowWhatYouDidLastSummer = new Movie("Horror", "I Know What You Did Last Summer");
+             
+            Movie StarWarsEpisode1PhantomMenace = new Movie("Scifi",  "Star Wars: Episode 1: The Phantom Menace");
+            Movie Alien = new Movie("Scifi", "Alien");
+            Movie Tremors = new Movie("Scifi", "Tremors");
+            Movie Predator = new Movie("Scifi", "Predator");
+            Movie ASpaceOdyssey = new Movie("Scifi", "A Space Odyssey");
+             
+
+            Console.WriteLine("Please enter a category:");
+            string input = Console.ReadLine();
+
+            List<Movie> Animated = new List<Movie>
+            {
+                TheLionKing, ABugsLife, FindingNemo, ToyStory, Aladdin
+            };
+
+            List<Movie> Drama = new List<Movie>
+            {
+                GoneWithTheWind, SavingPrivateRyan, TheGreatGatsby, ARiverRunsThroughIt, TheColorPurple
+            };
+
+            List<Movie> Horror = new List<Movie>
+            {
+               Halloween, ANightmareOnElmStreet, Fridaythe13th, Hellraiser, IknowWhatYouDidLastSummer
+            };
+            List<Movie> Scifi = new List<Movie>
+            {
+               StarWarsEpisode1PhantomMenace, Alien, Tremors, Predator, ASpaceOdyssey
+            };
+
+
+            foreach (Movie category in Animated)
+            {
+                category.PrintInfo();
+            }
+
+        }
+    }
+}
